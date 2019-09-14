810471acc9a8239d8655b52980a22a2f4fc7a096
John Doe <johndoe@example.com>
2019-09-14 12:41:01 -0500

    Implementación de ProductBusiness

---
+++ src/main/java/com/hampcode/business/impl/ProductBusiness.java
@@ -0,0 +1,43 @@
+package com.hampcode.business.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+
+import com.hampcode.business.IProductBusiness;
+import com.hampcode.model.entity.Product;
+import com.hampcode.model.repository.IProductRepository;
+
+@Named
+public class ProductBusiness implements IProductBusiness {
+
+	@Inject
+	private IProductRepository productRepository;
+	
+	@Transactional
+	@Override
+	public Long insert(Product entity) throws Exception {
+		return productRepository.insert(entity);
+	}
+
+	@Transactional
+	@Override
+	public Long update(Product entity) throws Exception {
+		return productRepository.update(entity);
+	}
+
+	@Override
+	public List<Product> getAll() throws Exception {
+		return productRepository.findAll();
+	}
+
+	@Override
+	public Optional<Product> getOne(Product entity) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
